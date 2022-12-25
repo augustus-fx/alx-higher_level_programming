f61b07c0adf175c2c769bafe2a40032c77db7573
augustus-fx <agbolanaugustine@gmail.com>
2022-12-25 21:38:35 +0000

    0. Squared simple

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-def safe_print_list_integers(my_list=[]. x=0):
+def safe_print_list_integers(my_list=[], x=0):
     """Print the first x element of a list that are integers.
 
     Args:
-	my_list (list): The list to print elements from,
+	my_list (list): The list to print elements from.
 	x (int): The number of elements of my_list to print.
 
     Returns:
@@ -12,11 +12,10 @@ def safe_print_list_integers(my_list=[]. x=0):
     """
     ret = 0
     for i in range(0, x):
-	try:
-	    print("{:d}".format(my_list[i]), end="")
-	    ret += 1
-	except (ValueError, TypeError):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            ret += 1
+        except (ValueError, TypeError):
 	    continue
     print("")
     return (ret)
-
