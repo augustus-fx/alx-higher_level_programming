12c5b7e8e5df4e846538516479bbc1f00f004980
augustus-fx <agbolanaugustine@gmail.com>
2022-12-26 12:41:27 +0000

    Safe printing integers

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -12,5 +12,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return (True)
-    except (TypeError, valueError):
+    except (TypeError, ValueError):
         return (False)
