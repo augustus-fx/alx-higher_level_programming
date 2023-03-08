2c27cfd0972ed1b9757b89bc75fd0d5fab0ae360
Augustine Agbolan <agbolanaugustine@gmail.com>
2023-03-08 01:20:18 +0000

    task 3 and 10 modified

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# 12-student.py
 """Defines a class Student."""
 
 
@@ -16,12 +17,12 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to-json(self, attrs=None):
-        """Get a dictionary representation of the student.
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
         If attrs is a list of strings, represents only those attributes
         included in the list.
         Args:
-            attrs (list): (Optional) The attributs to represent.
+            attrs (list): (Optional) The attributes to represent.
         """
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
