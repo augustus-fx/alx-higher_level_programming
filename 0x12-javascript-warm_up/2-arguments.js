ccace89f9879e427f91dda628cc3a0146a58f418
augustus-fx <agbolanaugustine@gmail.com>
2023-03-15 07:06:53 +0000

    bug fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 
 const len = process.argv.length;
-
-if (len ===  2) {
-  console.log('Argument' + (len ===  3 ? 's' : ' ') + 'found');
-} else  {
-  console.log('No argument ');
-}
+argCount <= 2 && console.log('No argument');
+argCount === 3 && console.log('Argument found');
+argCount > 3 && console.log('Arguments found');
